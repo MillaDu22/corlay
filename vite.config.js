40486690a1fb34546baebabc6d551b7b64b15e70
Milla22 <marti.ludmilla@orange.fr>
2025-01-28 21:06:40 +0100

    vite;config maj

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
-  build: {
-    outDir: 'dist',  // Assurez-vous que Vite génère le build dans le répertoire "dist"
-  },
   plugins: [react()],
 })
